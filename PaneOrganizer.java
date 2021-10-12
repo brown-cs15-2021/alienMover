c9070be263902c765f132d39447ec9eae7f73df1
Will Buerger <william_buerger@brown.edu>
2021-10-12 15:23:59 -0400

    Make alien instance var

--- PaneOrganizer.java
+++ PaneOrganizer.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.Pane;
 public class PaneOrganizer {
 
     private BorderPane root;
+    private Alien alien;
 
     public PaneOrganizer() {
         this.root = new BorderPane();
@@ -17,18 +18,18 @@ public class PaneOrganizer {
         this.root.setCenter(alienPane);
         HBox buttonPane = new HBox();
         this.root.setBottom(buttonPane);
-        Alien alien = new Alien(alienPane);
-        this.setUpButtons(buttonPane, alien);
+        this.alien = new Alien(alienPane);
+        this.setUpButtons(buttonPane);
     }
 
-    private void setUpButtons(HBox buttonPane, Alien alien) {
+    private void setUpButtons(HBox buttonPane) {
         Button b1 = new Button("Move Left!");
         Button b2 = new Button("Move Right!");
         buttonPane.getChildren().addAll(b1, b2);
         buttonPane.setSpacing(Constants.BUTTON_SPACING);
         buttonPane.setAlignment(Pos.CENTER);
-        b1.setOnAction((ActionEvent e) -> alien.moveLeft());
-        b2.setOnAction((ActionEvent e) -> alien.moveRight());
+        b1.setOnAction((ActionEvent e) -> this.alien.moveLeft());
+        b2.setOnAction((ActionEvent e) -> this.alien.moveRight());
     }
 
     public Pane getRoot() {
